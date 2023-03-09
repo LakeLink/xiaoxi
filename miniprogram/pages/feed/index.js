f7faeb53cb9701bd5299f753cd8d8400433fc3ee
48648666 <oncwnuLursR5PuSZSGTBM520UtA4@git.weixin.qq.com>
2023-03-09 14:16:31 +0800

    分享及分享

---
+++ miniprogram/pages/feed/index.js
@@ -0,0 +1,72 @@
+// pages/feed/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        imgUrl: "https://img01.yzcdn.cn/vant/tree.jpg",
+        imgUrlheart:"爱心.png",
+        imgUrlshare:"分享.png",
+        title: "标题1",
+        number_of_likes:"11",
+        content:"这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容",
+        user:"2022.02.17 西湖大学（云谷校区）wxj"
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
