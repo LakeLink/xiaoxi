20e5c219a4654904a5130cd4136a4eb7ddb2d2cc
飞得更高 <oncwnuNryw7E0qqclkndTlPZP83Q@git.weixin.qq.com>
2023-03-09 17:44:21 +0800

    添加 together 页面

---
+++ miniprogram/pages/feed/together.js
@@ -0,0 +1,70 @@
+// pages/feed/together.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        imgUrl: "https://blog.playo.co/wp-content/uploads/2019/02/shutterstock_794204539-scaled.jpg",
+        title: "羽毛球",
+        number_of_likes:"11",
+        content:"17号 艺哥和胡印良品约定大战三百回合，如果艺哥输了就得在书院献唱一曲。来观战的速报到👇",
+        user:"@wxj"
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
