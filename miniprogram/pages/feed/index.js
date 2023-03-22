81657e50565d912b5699cb3b49970041c4f94df7
飞得更高 <oncwnuNryw7E0qqclkndTlPZP83Q@git.weixin.qq.com>
2023-03-22 17:56:49 +0800

    WeRunDetails 显示相对时间

--- miniprogram/pages/feed/index.js
+++ miniprogram/pages/feed/index.js
@@ -1,4 +1,12 @@
 // pages/feed/index.js
+const dayjs = require('dayjs')
+
+const relativeTime = require('@3rdparty/dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+
+require('@3rdparty/dayjs/locale/zh-cn')
+dayjs.locale('zh-cn')
+
 Page({
 
     /**
@@ -8,9 +16,9 @@ Page({
         weRunDetails: [],
         imgUrl: "https://blog.playo.co/wp-content/uploads/2019/02/shutterstock_794204539-scaled.jpg",
         title: "羽毛球",
-        number_of_likes:"11",
-        content:"今天艺哥和胡印良品大战三百回合，1：299艺哥惜败，无奈他只能在周末在书院献唱一曲。",
-        user:"@wxj"
+        number_of_likes: "11",
+        content: "今天艺哥和胡印良品大战三百回合，1：299艺哥惜败，无奈他只能在周末在书院献唱一曲。",
+        user: "@wxj"
     },
 
     /**
@@ -18,10 +26,14 @@ Page({
      */
     onLoad(options) {
         const db = wx.cloud.database()
-        db.collection('WeRunDetail').get().then(r => {
+        db.collection('WeRunDetails').get().then(r => {
+            r.data.forEach(e => {
+                e.when = dayjs(e.when).fromNow()
+            })
             this.setData({
                 weRunDetails: r.data
             })
+            console.log(this.data.weRunDetails)
         })
     },
 
