fbde67eba1666a1c2cedf9fe82f2e24205718a07
飞得更高 <oncwnuNryw7E0qqclkndTlPZP83Q@git.weixin.qq.com>
2023-03-29 14:43:39 +0800

    提升 picker 使用体验

--- miniprogram/pages/about/index.js
+++ miniprogram/pages/about/index.js
@@ -14,6 +14,7 @@ Page({
         collegeIndex: 0,
         collegeList: ["α", "β", "γ", "δ"],
         year: "",
+        age: "",
         avatarUrl: defaultAvatarUrl,
     },
     onChooseAvatar(e) {
