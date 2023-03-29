538eba1f8180554bb278151d9a861e0029065793
飞得更高 <oncwnuNryw7E0qqclkndTlPZP83Q@git.weixin.qq.com>
2023-03-29 14:24:42 +0800

    个人信息页

---
+++ miniprogram/pages/about/index.js
@@ -0,0 +1,89 @@
+// pages/about/index.js
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        nickname: "123",
+        realname: "张三",
+        bio: "",
+        hobby: "",
+        college: "未选择",
+        collegeIndex: 0,
+        collegeList: ["α", "β", "γ", "δ"],
+        year: "请选择",
+        avatarUrl: defaultAvatarUrl,
+    },
+    onChooseAvatar(e) {
+        const {
+            avatarUrl
+        } = e.detail
+        this.setData({
+            avatarUrl,
+        })
+    },
+    onPickerChange(e) {
+        const collegeIndex = e.detail.value, college = this.data.collegeList[e.detail.value]
+        this.setData({
+            collegeIndex, college
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
