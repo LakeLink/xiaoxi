785eae2e22020c9a61a313ca5bf3489bee9bdb5b
48648666 <oncwnuLursR5PuSZSGTBM520UtA4@git.weixin.qq.com>
2023-04-02 10:06:20 +0800

    个人信息的保存更新以及读取（未封装）
    没写个人界面

--- miniprogram/pages/about/index.js
+++ miniprogram/pages/about/index.js
@@ -6,6 +6,7 @@ Page({
      * 页面的初始数据
      */
     data: {
+        posttime: "",
         nickname: "123",
         realname: "张三",
         bio: "",
@@ -25,32 +26,181 @@ Page({
             avatarUrl,
         })
     },
-    onPickerChange(e) {
-        const collegeIndex = e.detail.value, college = this.data.collegeList[e.detail.value]
+    onPickerChange:function (e){
+//        const collegeIndex = e.detail.value, college = this.data.collegeList[e.detail.value] 
+        console.log('picker_college发送选择改变，携带值为', e.detail.value)
         this.setData({
-            collegeIndex, college
+            collegeIndex: e.detail.value+1 ,
+            college : this.data.collegeList[e.detail.value]
         })
     },
-
+    InfSubmit: function(e){
+        console.log("submit event:", e.detail)
+        console.log("data:", this.data)
+        const db = wx.cloud.database()
+        const col = db.collection('WeUser_InfDetails')
+        var ttt=0
+        if (this.data.nickname=='123') {
+            console.log("error: you didn't write your nickname!")
+            wx.showToast({
+                title: '信息不完全',
+                icon: 'error'
+            })
+            ttt=1
+        }
+        if (this.data.realname=='张三') {
+            console.log("error: you didn't write your realname!")
+            wx.showToast({
+                title: '信息不完全',
+                icon: 'error'
+            })
+            ttt=1
+        }
+        if (this.data.hobby=='') {
+            console.log("error: you didn't write your hobby!")
+            wx.showToast({
+                title: '信息不完全',
+                icon: 'error'
+            })
+            ttt=1
+        }
+        if (this.data.collegeIndex==0) {
+            console.log("error: you didn't choose your college!")
+            wx.showToast({
+                title: '信息不完全',
+                icon: 'error'
+            })
+            ttt=1
+        }
+        if (ttt==1) {
+            console.log("stop")
+            return
+        }
+        else{
+            console.log("continue")
+        }
+        var If_fill
+        wx.cloud.callFunction({
+            name: 'openid',
+            complete: res => {
+              //console.log('callFunction test result: ',res.result.userInfo.openId)
+              col.where({
+                _openid: res.result.userInfo.openId,
+              }).count().then(ress=>{
+                console.log(ress)
+                if(ress.total==1){
+                    col.where({
+                        _openid: res.result.userInfo.openId,
+                    })
+                    .update({
+                        data: {
+                            posttime: db.serverDate(),
+                            nickname: this.data.nickname,
+                            realname: this.data.realname,
+                            biography: this.data.bio,
+                            hobby: this.data.hobby,
+                            college: this.data.college,
+                            collegeIndex: this.data.collegeIndex,
+                            year: this.data.year,
+                            age:this.data.age
+                        }
+                    }).then (ress=> {
+                        console.log('text',ress)
+                        })
+                    }
+                    else{
+                        col.add({
+                            data: {
+                                posttime: db.serverDate(),
+                                nickname: this.data.nickname,
+                                realname: this.data.realname,
+                                biography: this.data.bio,
+                                hobby: this.data.hobby,
+                                college: this.data.college,
+                                collegeIndex: this.data.collegeIndex,
+                                year: this.data.year,
+                                age:this.data.age
+                            }
+                        }).then(res=>{
+                            console.log(res)
+                        })
+                    }
+                
+              })
+              
+              }
+                
+            })
+            
+          },
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad(options) {
+    
 
+    onLoad(options) {
+        const db = wx.cloud.database()
+        const col = db.collection('WeUser_InfDetails')
+        const da =this
+        
+        wx.cloud.callFunction({
+            name: 'openid',
+            complete: res => {
+              //console.log('callFunction test result: ',res.result.userInfo.openId)
+              col.where({
+                _openid: res.result.userInfo.openId,
+              }).count().then(ress=>{
+                console.log(ress)
+              })
+              col.where({
+                _openid: res.result.userInfo.openId,
+              })
+              .get({
+                success: function(ress) {
+                  // res.data 是包含以上定义的两条记录的数组
+                  //console.log('text',ress.data)
+                  da.setData(ress.data[0])   
+                  /*da.setData({
+                        nickname : ress.data[0].nickname,
+                        realname : ress.data[0].realname,
+                        bio : ress.data[0].biography,
+                        hobby : ress.data[0].hobby,
+                        college : ress.data[0].college,
+                        collegeIndex : ress.data[0].collegeIndex,
+                        year : ress.data[0].year,
+                        age : ress.data[0].age
+                    })*/
+                    /*
+                    da.nickname = ress.data[0].nickname,
+                    da.realname = ress.data[0].realname,
+                    da.bio = ress.data[0].biography,
+                    da.hobby = ress.data[0].hobby,
+                    da.college = ress.data[0].college,
+                    da.collegeIndex = ress.data[0].collegeIndex,
+                    da.year = ress.data[0].year,
+                    da.age = ress.data[0].age
+                    console.log('text',ress.data[0].nickname,'datt',da)
+                    */
+                }
+              })
+            }
+            
+          })
+          //console.log('text','datt',da)
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady() {
-
+        
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow() {
-
+    
     },
 
     /**
