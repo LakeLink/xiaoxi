ce4dadec9bae3ccb525964b78740bf37c2002ca3
Fly Higher <daniel_sy@live.com>
2023-04-04 11:10:37 +0800

    style: feed-index page

--- miniprogram/pages/feed/index.js
+++ miniprogram/pages/feed/index.js
@@ -13,12 +13,7 @@ Page({
      * 页面的初始数据
      */
     data: {
-        weRunDetails: [],
-        imgUrl: "https://blog.playo.co/wp-content/uploads/2019/02/shutterstock_794204539-scaled.jpg",
-        title: "羽毛球",
-        number_of_likes: "11",
-        content: "今天艺哥和胡印良品大战三百回合，1：299艺哥惜败，无奈他只能在周末在书院献唱一曲。",
-        user: "@wxj"
+        weRunDetails: []
     },
 
     async refresh() {
