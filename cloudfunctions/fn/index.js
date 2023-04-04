b37044dd3004ad56b54a402414253990e9cd2d1f
Fly Higher <daniel_sy@live.com>
2023-04-04 11:08:31 +0800

    cloudfunction: combine together func

--- cloudfunctions/fn/index.js
+++ cloudfunctions/fn/index.js
@@ -1,14 +1,13 @@
-const joinTogether = require('./joinTogether')
-const getTogether = require('./getTogether')
+const together = require('./together')
 const user = require('./user')
 
 // 云函数入口函数
 exports.main = async function (event, context) {
     switch (event.type) {
         case 'joinTogether':
-            return await joinTogether.main(event, context)
+            return await together.join(event, context)
         case 'getTogether':
-            return await getTogether.main(event, context)
+            return await together.get(event, context)
         case 'getUser':
             return await user.read(event, context)
         case 'saveUser':
--- cloudfunctions/fn/together.js
+++ cloudfunctions/fn/together.js
@@ -5,7 +5,81 @@ cloud.init({
 });
 
 // 获取openId云函数入口函数
-exports.main = async (event, context) => {
+exports.join = async (event, context) => {
+    // 获取基础信息
+    const {
+        ENV,
+        OPENID,
+        APPID
+    } = cloud.getWXContext()
+    console.log(OPENID)
+    const db = cloud.database()
+    const col = db.collection('TogetherDetails')
+    const _ = db.command
+    const $ = _.aggregate
+
+    let full = false
+    let r = await col.where(_.and([{
+                _id: event.id
+            },
+            // 没报名
+            _.nor([{
+                    partners: OPENID
+                },
+                {
+                    waitList: OPENID
+                }
+            ]),
+            //人数没超
+            _.expr(
+                $.lt([
+                    $.size('$partners'),
+                    '$limit'
+                ])
+            )
+        ]))
+        .update({
+            data: {
+                partners: _.push(OPENID)
+            }
+        })
+        .then(r => {
+            // 报满了
+            if (!r || r.stats.updated == 0) {
+                return col.where(_.and([{
+                        _id: event.id
+                    },
+                    // 没报名
+                    _.nor([{
+                            partners: OPENID
+                        },
+                        {
+                            waitList: OPENID
+                        }
+                    ]),
+                    //人数超了
+                    _.expr(
+                        $.gte([
+                            $.size('$partners'),
+                            '$limit'
+                        ])
+                    )
+                ]))
+                .update({
+                    data: {
+                        waitList: _.push(OPENID)
+                    }
+                })
+            } else return r
+        })
+    return {
+        r,
+        full
+    }
+};
+
+// 获取openId云函数入口函数
+exports.get = async (event, context) => {
     // 获取基础信息
     const {
         ENV,
