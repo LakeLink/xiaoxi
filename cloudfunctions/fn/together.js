f865e1c0d59f51e1fe91913bec3018ee93071e19
飞得更高 <oncwnuNryw7E0qqclkndTlPZP83Q@git.weixin.qq.com>
2023-04-16 10:37:32 +0800

    add: notification

--- cloudfunctions/fn/together.js
+++ cloudfunctions/fn/together.js
@@ -1,5 +1,6 @@
 const cloud = require('wx-server-sdk');
 const quickAction = require('./quickAction')
+const dayjs = require('dayjs')
 
 cloud.init({
     env: cloud.DYNAMIC_CURRENT_ENV
@@ -160,11 +161,36 @@ exports.quit = async (event, context) => {
     const _ = db.command
     const $ = _.aggregate
 
-    return (await col.doc(event.id).update({
+    const item = await col.doc(event.id).get().then(r => r.data)
+    // console.log(item)
+    const r = (await col.doc(event.id).update({
         data: {
             partners: _.pull(OPENID)
         }
     })).stats
+
+    if (r.updated && item.partners.length > item.limit && item.partners.indexOf(OPENID) < item.limit) {
+        cloud.openapi.subscribeMessage.send({
+            templateId: 'Denmjkfh0o9B8LlimVu31V5GooHI2mM64ucAXj_3d1Y',
+            page: `pages/feed/together?id=${item._id}`,
+            touser: OPENID,
+            data: {
+                thing2: {
+                    value: item.sportsType
+                },
+                time4: {
+                    value: dayjs(item.scheduledAt).format('YYYY/MM/DD HH:mm')
+                },
+                thing5: {
+                    value: item.location
+                },
+                thing6: {
+                    value: '候补成功，您已加入运动队伍'
+                }
+            }
+        })
+    }
+    return r
 }
 
 exports.createActivityId = async (event, context) => {
@@ -198,4 +224,29 @@ exports.createActivityId = async (event, context) => {
         limit: doc.limit,
         current: doc.partners.length
     }
+}
+
+exports.afterDelete = async (event, context) => {
+    console.log(event)
+    event.partners.forEach(e => {
+        cloud.openapi.subscribeMessage.send({
+            templateId: 'qHaLq87tBwA8RWHuAw7ZqW9pETn0pvx_OPYRe4uJN10',
+            page: 'pages/feed/together',
+            touser: e,
+            data: {
+                thing1: {
+                    value: event.sportsType
+                },
+                date2: {
+                    value: dayjs(event.scheduledAt).format('YYYY/MM/DD HH:mm')
+                },
+                thing3: {
+                    value: event.location
+                },
+                thing11: {
+                    value: '发起者已取消该活动，去看看别的吧'
+                }
+            }
+        })
+    })
 }
