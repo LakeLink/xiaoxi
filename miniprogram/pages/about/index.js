688b8f20ca824a0235ae8715baa7ba12a6c1fd86
Fly Higher <daniel_sy@live.com>
2023-11-03 02:06:59 +0800

    update: tabbar

--- miniprogram/pages/about/index.js
+++ miniprogram/pages/about/index.js
@@ -361,6 +361,12 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow() {
+        if (typeof this.getTabBar === 'function' &&
+            this.getTabBar()) {
+            this.getTabBar().setData({
+                value: '/pages/about/index'
+            })
+        }
         this.refreshWeRunPermission()
         this.refresh()
     },
